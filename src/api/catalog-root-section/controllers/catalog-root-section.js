50bcc364da7d3eecb3250449b98bec96475741f1
oLNidfwworld <kuvetov@bk.ru>
2024-03-13 00:07:01 +0300

    controllers root section change response

--- src/api/catalog-root-section/controllers/catalog-root-section.js
+++ src/api/catalog-root-section/controllers/catalog-root-section.js
@@ -1,27 +1,53 @@
-'use strict';
+"use strict";
 
 /**
  * catalog-section controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
- 
+const { createCoreController } = require("@strapi/strapi").factories;
 
 module.exports = createCoreController(
-    "api::catalog-root-section.catalog-root-section",
-    ({ strapi }) => ({
-      async findOne(ctx) {  
-          const { slug } = ctx.params; 
-          const { query } = ctx;  
-          if (!query.filters) query.filters = {}
-          query.filters.slug = { '$eq': slug }
-          query.populate = [
-            'catalog_sections'
-          ];
-          const entity = await strapi.service('api::catalog-root-section.catalog-root-section').find(query);
-          const { results } = await this.sanitizeOutput(entity, ctx); 
-          return this.transformResponse(results[0]);
-      },
-    })
-  );
-  
+  "api::catalog-root-section.catalog-root-section",
+  ({ strapi }) => ({
+    async findOne(ctx) {
+      const { slug } = ctx.params;
+      const { query } = ctx;
+      if (!query.filters) query.filters = {};
+      query.filters.slug = { $eq: slug };
+
+      //   query.populate = [
+      //     'catalog_sections'
+      //   ];
+
+      // ?populate[catalog_sections][populate]=catalog_items
+
+      query.populate = {
+        catalog_sections: {
+          populate: "catalog_items",
+        },
+      };
+
+      const entity = await strapi
+        .service("api::catalog-root-section.catalog-root-section")
+        .find(query);
+
+      const { results } = await this.sanitizeOutput(entity, ctx);
+      const response = this.transformResponse(results[0]);
+
+      let itemsArray = [];
+      if (response?.data) {
+        response.data.attributes.catalog_sections.data.map((sectionObject) => {
+          sectionObject.attributes.catalog_items.data.map((itemsObject) =>
+            itemsArray.push(itemsObject)
+          );
+          delete sectionObject.attributes.catalog_items;
+        });
+        response.data.attributes.catalog_items = {
+          data: itemsArray,
+        };
+      }
+
+      return response;
+    },
+  })
+);
