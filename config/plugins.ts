b4db65989fe700f0385a762098ee5d934e9dfbd7
oLNidfwworld <kuvetov@bk.ru>
2024-12-27 16:29:20 +0300

    some changes

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,6 +1,10 @@
 export default ({ env }) => ({
     'users-permissions': {
       config: {
+        register : {
+          allowedFields: ['name', 'secondName'],
+          requiredFields: ['name', 'secondName']
+        },
         jwt: {
           expiresIn: '30d', 
         },
---
+++ src/extensions/users-permissions/strapi-server.ts
@@ -0,0 +1,56 @@
+export default function(plugin){
+    console.log(plugin);
+  //   plugin.controllers.user.addToCart = async (ctx) => {
+  //     if (!ctx.state.user || !ctx.state.user.id) {
+  //       return (ctx.response.status = 401);
+  //     } 
+  
+  //     if ( !('cart' in ctx.request.body) ) {
+  //         return (ctx.response.status = 401);
+  //     }
+  
+  //     const cartItem = ctx.request.body.cart;
+  
+  //     await strapi
+  //       .query("plugin::users-permissions.user")
+  //       .update({
+  //         where: { id: ctx.state.user.id },
+  //         data: ctx.request.body,
+  //       })
+  //       .then((res) => {
+  //         ctx.response.status = 200;
+  //       });
+  //   }; 
+  
+  //   plugin.routes["content-api"].routes.push({
+  //     method: "PUT",
+  //     path: "/user/cart/add",
+  //     handler: "user.addToCart",
+  //     config: {
+  //       prefix: "",
+  //       policies: [],
+  //     },
+  //   });
+    const register = plugin.controllers.auth.register;
+
+    plugin.controllers.auth.register = async (ctx) => { 
+      await register(ctx)
+    
+      // then get userId from register response 
+      const userId = ctx.response.body.user.id
+      console.log(userId);
+      // save custom data registration with update service
+      const user = await strapi.entityService.update('plugin::users-permissions.user', userId, {
+        data: {
+          name: ctx.request.body.name,
+          seconName: ctx.request.body.seconName,
+       },
+      });
+    
+      // return the response as you want
+      ctx.body = { data: `Congrats ${user.name}! your account has registered successfully.` }
+    }
+  
+    return plugin;
+  };
+  
